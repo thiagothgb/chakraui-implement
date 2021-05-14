b16ae3d61ecc8e5f46be69b226f1c993d22594b1
Thiago Brandao <thiagothgb@gmail.com>
2021-05-14 16:53:02 -0400

    Refatoraçao do header e sidebar

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,34 @@
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Logo } from "./Logo";
+import { NotificationNav } from "./NotificationNav";
+import { Profile } from "./Profile";
+import { SearchBox } from "./SearchBox";
+
+export function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
+  return (
+    <Flex
+      w="100%"
+      as="header"
+      maxWidth={1480}
+      h="20"
+      mt="4"
+      mx="auto"
+      px="6"
+      align="center"
+    >
+      <Logo />
+
+      {isWideVersion && <SearchBox />}
+
+      <Flex align="center" ml="auto">
+        <NotificationNav />
+        <Profile showProfileData={isWideVersion} />
+      </Flex>
+    </Flex>
+  );
+}
